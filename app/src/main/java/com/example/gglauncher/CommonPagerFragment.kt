a634f0b09b137ccad25e90c8e56c97311bd78c70
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2020-05-30 20:12:54 +0900

    Set scrollable top view

---
+++ app/src/main/java/com/example/gglauncher/CommonPagerFragment.kt
@@ -0,0 +1,55 @@
+package com.example.gglauncher
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+
+class CommonPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_common_pager, container, false)
+    }
+
+}
+
+private const val START_POSITION = 2
+private var FRAGMENTS = arrayOf(ClockFragment(), ClockFragment(), ClockFragment(), ClockFragment(), ClockFragment())
+
+class CommonPagerActivity : FragmentActivity() {
+
+    private lateinit var mPager: ViewPager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_common_pager)
+
+        mPager = findViewById(R.id.viewPager)
+
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
+        mPager.currentItem = START_POSITION
+    }
+
+    override fun onBackPressed() {
+        if (mPager.currentItem == 0) {
+            super.onBackPressed()
+        } else {
+            mPager.currentItem = mPager.currentItem - 1
+        }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+        override fun getCount(): Int = FRAGMENTS.size
+        override fun getItem(position: Int): Fragment = FRAGMENTS.get(position)
+    }
+}
