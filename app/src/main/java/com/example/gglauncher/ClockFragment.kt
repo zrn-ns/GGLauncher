bdf1fc5b6e8066b7f75b990b7263675c7bf90a8c
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2020-05-30 18:57:38 +0900

    Show Clock

---
+++ app/src/main/java/com/example/gglauncher/ClockFragment.kt
@@ -0,0 +1,23 @@
+package com.example.gglauncher
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ClockFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_clock, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
