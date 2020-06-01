7b6f056159d98a027bde80c9cef506093415b8e9
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2020-06-01 11:57:51 +0900

    Add gallery sample app to launcher

---
+++ app/src/main/java/com/zrnns/gglauncher/gallery_app/GalleryPageFragment.kt
@@ -0,0 +1,24 @@
+package com.zrnns.gglauncher.camera_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.zrnns.gglauncher.R
+import com.zrnns.gglauncher.core.StandardAppPageFragment
+
+class GalleryPageFragment : StandardAppPageFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+
+        super.set(R.drawable.ic_app_icon_album, R.string.app_name_gallery)
+
+        return view
+    }
+}
