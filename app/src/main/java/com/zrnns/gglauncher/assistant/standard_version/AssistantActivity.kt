6598ce7e2b6ae7af92e2bbb0dd01f90b17bf31ba
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2020-06-06 22:03:33 +0900

    Prepare for showing image list on google assistant view

--- app/src/main/java/com/zrnns/gglauncher/assistant/standard_version/AssistantActivity.kt
+++ app/src/main/java/com/zrnns/gglauncher/assistant/standard_version/AssistantActivity.kt
@@ -1,35 +1,34 @@
 package com.zrnns.gglauncher.assistant.standard_version
 
 import android.graphics.Color
-import android.opengl.Visibility
 import android.os.Bundle
-import android.util.Base64
 import android.view.MotionEvent
 import android.view.View
-import android.webkit.WebView
 import android.widget.*
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import coil.api.clear
 import coil.api.load
 import com.zrnns.gglauncher.R
+import com.zrnns.gglauncher.core.CommonPagerActivity
 import com.zrnns.gglauncher.core.GlassGestureDetector
+import com.zrnns.gglauncher.core.observer.NonNullLiveData
 import java.util.*
 
-class AssistantActivity : AppCompatActivity(), GlassGestureDetector.OnGestureListener {
+class AssistantActivity : CommonPagerActivity() {
+
+    private var firstPageFragment = AssistantFirstPageFragment()
+    override var startPosition: Int = 0
+    override var fragments = NonNullLiveData<List<Fragment>>(listOf(firstPageFragment))
 
     private lateinit var viewModel: AssistantActivityViewModel
     private val glassGestureDetector: GlassGestureDetector by lazy { GlassGestureDetector(this, this) }
 
-    private lateinit var contentArea: LinearLayout
-    private lateinit var resultImageView: ImageView
-    private lateinit var resultTextView: TextView
-    private lateinit var inputMessageView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupViews()
+        viewModel = AssistantActivityViewModel(applicationContext)
+        firstPageFragment.viewModel = viewModel
+
         setupObservers()
 
         viewModel.onCreateAction()
@@ -58,49 +57,10 @@ class AssistantActivity : AppCompatActivity(), GlassGestureDetector.OnGestureLis
         } else super.dispatchTouchEvent(event)
     }
 
-    private fun setupViews() {
-        setContentView(R.layout.activity_assistant)
-
-        contentArea = findViewById(R.id.contentArea)
-        resultImageView = findViewById(R.id.resultImageView)
-        resultTextView = findViewById(R.id.resultTextView)
-        inputMessageView = findViewById(R.id.inputMessageView)
-    }
-
     private fun setupObservers() {
-        viewModel =
-            AssistantActivityViewModel(
-                applicationContext
-            )
-
-        val contentObserver = Observer<AssistantActivityViewModel.AssistantResultContent?>() {
-            it?.let {
-                contentArea.visibility = View.VISIBLE
-                resultTextView.text = it.text
-                resultTextView.visibility = if (it.text.isNullOrEmpty()) View.GONE else View.VISIBLE
-                resultImageView.load(it.firstImageUrl)
-                resultImageView.visibility = if (it.firstImageUrl == null)  View.GONE else View.VISIBLE
-            } ?: run {
-                contentArea.visibility = View.GONE
-            }
-        }
-        val messageObserver = Observer<String>() {
-            inputMessageView.text = it
-        }
-        val messageColorObserver = Observer<Color>() {
-            inputMessageView.setTextColor(it.toArgb())
-        }
-        val messageTextSizeObserver = Observer<Float>() {
-            inputMessageView.textSize = it
-        }
         val activityFinishTriggerObserver = Observer<UUID> {
             finish()
         }
-
-        viewModel.assistantResultContent.observe(this, contentObserver)
-        viewModel.message.observe(this, messageObserver)
-        viewModel.messageColor.observe(this, messageColorObserver)
-        viewModel.messageTextSize.observe(this, messageTextSizeObserver)
         viewModel.activityFinishTrigger.observe(this, activityFinishTriggerObserver)
     }
 }
---
+++ app/src/main/java/com/zrnns/gglauncher/assistant/standard_version/AssistantFirstPageFragment.kt
@@ -0,0 +1,69 @@
+package com.zrnns.gglauncher.assistant.standard_version
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import coil.api.load
+import com.zrnns.gglauncher.R
+
+class AssistantFirstPageFragment() : Fragment() {
+
+    private lateinit var contentArea: LinearLayout
+    private lateinit var resultImageView: ImageView
+    private lateinit var resultTextView: TextView
+    private lateinit var inputMessageView: TextView
+
+    var viewModel: AssistantActivityViewModel? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_assistant_first_page, container, false)
+        contentArea = view.findViewById(R.id.contentArea)
+        resultImageView = view.findViewById(R.id.resultImageView)
+        resultTextView = view.findViewById(R.id.resultTextView)
+        inputMessageView = view.findViewById(R.id.inputMessageView)
+
+        setupObservers()
+
+        return view
+    }
+
+    private fun setupObservers() {
+        val contentObserver = Observer<AssistantActivityViewModel.AssistantResultContent?>() {
+            it?.let {
+                contentArea.visibility = View.VISIBLE
+                resultTextView.text = it.text
+                resultTextView.visibility = if (it.text.isNullOrEmpty()) View.GONE else View.VISIBLE
+                resultImageView.load(it.firstImageUrl)
+                resultImageView.visibility = if (it.firstImageUrl == null)  View.GONE else View.VISIBLE
+            } ?: run {
+                contentArea.visibility = View.GONE
+            }
+        }
+        val messageObserver = Observer<String>() {
+            inputMessageView.text = it
+        }
+        val messageColorObserver = Observer<Color>() {
+            inputMessageView.setTextColor(it.toArgb())
+        }
+        val messageTextSizeObserver = Observer<Float>() {
+            inputMessageView.textSize = it
+        }
+
+        viewModel?.let {
+            it.assistantResultContent.observe(this, contentObserver)
+            it.message.observe(this, messageObserver)
+            it.messageColor.observe(this, messageColorObserver)
+            it.messageTextSize.observe(this, messageTextSizeObserver)
+        }
+    }
+}
