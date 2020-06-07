2182b17f98c3e63480d8c63c08f6d3fc2ed60384
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2020-06-07 18:40:55 +0900

    Change settings app class name to Glass-settings-app

--- app/src/main/java/com/zrnns/gglauncher/settings_app/PackageFunctions.kt
+++ app/src/main/java/com/zrnns/gglauncher/settings_app/PackageFunctions.kt
@@ -3,9 +3,10 @@ package com.zrnns.gglauncher.settings_app
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.provider.Settings
 import android.widget.Toast
 
+private const val PACKAGE_NAME = "com.google.android.glass.settings"
+private const val CLASS_NAME = "com.google.android.glass.settings.MainActivity"
 
 fun openAndroidSettingsActivity(fromActivity: Activity) {
     val intent = OpenAndroidSettingsIntent()
@@ -22,6 +23,9 @@ fun openAndroidSettingsActivity(fromActivity: Activity) {
 
 private class OpenAndroidSettingsIntent: Intent() {
     init {
-        action = Settings.ACTION_SETTINGS
+        setClassName(
+            PACKAGE_NAME,
+            CLASS_NAME
+        )
     }
 }
